49e4014b7cdc6ba40af8bc9cacb28a48b75c22c9
Dom Hernández <dom.hdez0210@gmail.com>
2020-06-11 13:27:21 -0400

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+xfce4-power-manager &
+nm-applet &
+volumeicon &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nitrogen --restore &
+flameshot &
+clipit &
+compton -b &
+redshift-gtk &
